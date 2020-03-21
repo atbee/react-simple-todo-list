f00c285cac5c4050acd1e9a8d46d64c996654e23
Athibet Prawane <atb@odds.team>
2020-03-21 18:40:20 +0700

    Add unique id

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,21 +2,22 @@ import React from 'react'
 import Header from './Header'
 import InputTodo from './InputTodo'
 import TodosList from './TodosList'
+import uuid from 'uuid'
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Deploy to live server",
         completed: false
       }
