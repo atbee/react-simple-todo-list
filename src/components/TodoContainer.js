62037bbae0cb40f2bafb40aee147c50a4f3fcd54
Athibet Prawane <atb@odds.team>
2020-03-21 13:58:31 +0700

    Update root message with TodoContainer

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hola atb</h1>
+                <p>I am in a React Component!</p>
+            </div>
+        )
+    }
+}
+export default TodoContainer
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const element = <h1>hola atb</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+import TodoContainer from './components/TodoContainer'
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
